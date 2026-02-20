a015d5e19b48b32f421797746b80d49e6ba1af4c
Dávilla Ferreira <davilla.souza1@aluno.ce.gov.br>
2026-02-20 08:29:38 -0300

    verifica_login.php

--- verifica_login.php
+++ verifica_login.php
@@ -11,9 +11,10 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) == 1) {
     $usuario_data = mysqli_fetch_assoc($result);
     $_SESSION['usuario'] = $usuario_data['usuario'];
-    $_SESSION['nivel'] = $usuario_data['nivel']; // nível de acesso
+    $_SESSION['nivel'] = $usuario_data['nivel'];
     header("Location: painel_funcionario.php");
 } else {
     echo "<script>alert('Usuário ou senha inválidos'); window.location='login.php';</script>";
 }
 ?>
+
